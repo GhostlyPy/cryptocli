08fc0248591ab9b057db41b1bd12fddd2c8d6bd4
trevor_borden@outlook.com <56496067+GhostlyPy@users.noreply.github.com>
2020-07-27 19:57:37 -0500

    Update crypto.js

--- bin/crypto.js
+++ bin/crypto.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+ // Packages used for CLI
+const program = require('commander');
+const pkg = require('../package.json');
 
-console.log('Hello from Cryptocli');
+// Grabs the current version from the JSON file & Adds a new command 'key' to the command list
+program
+    .version(pkg.version)
+    .command('key', 'Manage API Key -- https://nomics.com')
+    .parse(process.argv);
+
+// Used for testing extension
+// console.log('Hello from Cryptocli');
