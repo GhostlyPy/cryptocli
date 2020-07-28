eddada45bf713b4959d1ea9dfe8d1c1ba878dcf1
trevor_borden@outlook.com <56496067+GhostlyPy@users.noreply.github.com>
2020-07-27 21:53:47 -0500

    Update crypto-check.js

--- bin/crypto-check.js
+++ bin/crypto-check.js
@@ -1,8 +1,11 @@
 const program = require('commander');
+const check = require('../commands/check');
 
 program
   .command('price')
   .description('Check price of coins')
-  .action(() => console.log('Hello from Price!'));
+  .option('--crypto <type>', 'Add specific crypocurrency type in CSV format', 'BTC,ETH,XRP')
+  .option('--cur <currency>', 'Convert the current currency', 'USD')
+  .action(cmd => check.price(cmd));
 
 program.parse(process.argv);
