f12dd25c67f3b8a800ae26e6f36b48341f8027f8
trevor_borden@outlook.com <56496067+GhostlyPy@users.noreply.github.com>
2020-07-27 20:40:55 -0500

    Create key.js

---
+++ commands/key.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyManager = require('../lib/KeyManager');
+const {
+  isRequired
+} = require('../utils/validation');
+
+const key = {
+  async set() {
+    const keyManager = new KeyManager();
+    // Returns a promise. Used for input from user, used for setting the API key
+    const input = await inquirer.prompt([{
+      type: 'input',
+      name: 'key',
+      message: 'Enter API Key '.green + 'https://nomics.com',
+      validate: isRequired
+    }]);
+
+    const key = keyManager.setKey(input.key);
+
+    if (key) {
+      console.log('API Key Set!'.blue);
+    }
+  },
+  show() {
+    try {
+      const keyManager = new KeyManager();
+      const key = keyManager.getKey();
+
+      console.log('Current API Key: ', key.yellow);
+    } catch (err) {}
+  },
+  remove() {
+    console.log('Hello from remove');
+  }
+}
+
+module.exports = key;
