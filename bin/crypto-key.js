bb72a0a6a60321b32d20c7d85c4ab2777078f63c
trevor_borden@outlook.com <56496067+GhostlyPy@users.noreply.github.com>
2020-07-27 20:40:48 -0500

    Update crypto-key.js

--- bin/crypto-key.js
+++ bin/crypto-key.js
@@ -1,20 +1,21 @@
 // Packages used for CLI 'key' command
 const program = require('commander');
+const key = require('../commands/key');
 
-// Adds a new command 'set', and also adds a description and action for the command
+// Adds a new command 'set', which sets the API key
 program
 	.command('set')
 	.description('Set API Key -- Get at https://nomics.com')
-	.action(() => console.log('Hello from set'));
-
+	.action(key.set);
+// Adds new command for 'show', which will show the current API key being used
 program
 	.command('show')
 	.description('Show API Key')
-	.action(() => console.log('Hello from show'));
-
+	.action(key.show);
+// Adds new command for 'remove', which will remove the current API key being used
 program
 	.command('remove')
 	.description('Remove API Key')
-	.action(() => console.log('Hello from remove'));
+	.action(key.remove);
 
 program.parse(process.argv);
