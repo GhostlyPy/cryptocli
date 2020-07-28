29770cd729cc2809a71d9d7490faaf9abf3c81b7
trevor_borden@outlook.com <56496067+GhostlyPy@users.noreply.github.com>
2020-07-27 19:57:33 -0500

    Create crypto-key.js

---
+++ bin/crypto-key.js
@@ -0,0 +1,10 @@
+// Packages used for CLI 'key' command
+const program = require('commander');
+
+// Adds a new command 'set', and also adds a description and action for the command
+program
+    .command('set')
+    .description('Set API Key -- Get at https://nomics.com')
+    .action(() => console.log('Hello from set'));
+
+program.parse(process.argv);
