25eff1c14c48c18f46f92caa0b0e6584469beddd
trevor_borden@outlook.com <56496067+GhostlyPy@users.noreply.github.com>
2020-07-27 20:12:01 -0500

    Update crypto-key.js

--- bin/crypto-key.js
+++ bin/crypto-key.js
@@ -7,4 +7,14 @@ program
 	.description("Set API Key -- Get at https://nomics.com")
 	.action(() => console.log("Hello from set"));
 
+program
+	.command("show")
+	.description("Show API Key")
+	.action(() => console.log("Hello from show"));
+
+program
+	.command("remove")
+	.description("Remove API Key")
+	.action(() => console.log("Hello from remove"));
+
 program.parse(process.argv);
